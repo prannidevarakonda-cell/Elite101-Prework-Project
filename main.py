8c59c4a2f2e6c8e8cab1de5411cef649ad892248
Pranay Devarakonda <pranni.devarakonda@gmail.com>
2025-09-23 20:04:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print("Welcome to the Pi Cafe Chatbot!")
-name = input("Please enter your name:")
-age = input("Hello "  + name + ", What is your age?")
+name = input("Please enter your name: ")
+
+# Convert age to an integer
+age = int(input("Hello " + name + ", what is your age? "))
+
+if age < 13:
+    kids_menu = input("What would you like on the kids menu? ")
+    print("Great choice! We'll bring you the " + kids_menu + ".")
+else:
+    normal_menu = input("What would you like to order? ")
+    print("Excellent! We'll prepare your " + normal_menu + ".")
